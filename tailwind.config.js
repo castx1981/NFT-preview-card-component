178ff77d0e7689fda1639ee09ffc100a22895911
Xiaoliang Luan <helix_ogg@hotmail.com>
2023-04-16 13:07:09 +0800

    first draft

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['Outfit', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        // primary
+        'soft-blue': 'hsl(215, 51%, 70%)',
+        'cyan': 'hsl(178, 100%, 50%)',
+        // neutral
+        'main-dark-blue': 'hsl(217, 54%, 11%)',
+        'card-dark-blue': 'hsl(216, 50%, 16%)',
+        'line-dark-blue': 'hsl(215, 32%, 27%)',
+      },
+      backgroundImage: {
+        'view': "url('images/icon-view.svg')"
+      },
+      boxShadow: {
+        'card': '0 44px 60px -5px hsl(217, 88%, 4%)',
+      }
+    }
   },
   plugins: [],
 }
-
